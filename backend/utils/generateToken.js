e9e649d42569f25f23927c1bd234f4bcc9f1032f
Abiram <uthayakumarabiram@gmail.com>
2024-03-04 10:24:58 +0530

    token fixed

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken =(res,userId)=>{
 
   res.cookie('jwt',token,{
     httpOnly:true,
-    secure:process.env.NODE_ENV !== "development",
+    secure:process.env.NODE_ENV !== "production",
     sameSite:'strict',
     maxAge:30*24*60*60*1000
   })
